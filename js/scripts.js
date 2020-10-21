9c85777c7c99734029209bab2d246d2f8bdd0eb9
Eric Stratton <strattonericj@gmail.com>
2020-10-21 13:50:54 -0700

    add bootstrap jQuery, reformat calculator functions, refine html structure
    Co-authored-by: Erica Woodmansee <ewoodmansee@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,27 @@
 // Buisness logic
-function add(number1, number2) {
+const add = function (number1, number2) {
   return number1 + number2;
-}
+};
 
-function sub(number1, number2) {
+const sub = function (number1, number2) {
   return number1 - number2;
-}
+};
 
-function multi(number1, number2) {
+const multi = function (number1, number2) {
   return number1 * number2;
-}
+};
 
-function div(number1, number2) {
+const div = function (number1, number2) {
   return number1 / number2;
-}
+};
 
 // User Interface Logic
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("enter a second number:"));
-const result = div(number1, number2);
-alert(result);
+$(document).ready(function () {
+  $("form#add").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
+});
+
